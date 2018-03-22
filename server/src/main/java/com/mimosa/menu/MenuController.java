a2472e1ae229cb0cce8119ec0a3350227af90b34
MilBia <218295@student.pwr.wroc.pl>
2018-03-22 18:06:04 +0100

    basic menu modul

---
+++ server/src/main/java/com/mimosa/menu/MenuController.java
@@ -0,0 +1,49 @@
+package com.mimosa.menu;
+
+import com.mimosa.menu.model.Menu;
+import com.mimosa.utilities.MergeTool;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/menu")
+public class MenuController {
+
+    @Autowired
+    public MenuService menuService;
+
+    @RequestMapping(value = "{id}",method = RequestMethod.GET)
+    public ResponseEntity<Menu> getMenu(@PathVariable Integer id){
+        return menuService.getMenuById(id).map(u -> new ResponseEntity<>(u, HttpStatus.OK)).
+                orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Menu> addMenu(@RequestBody Menu menu){
+        try {
+            return new ResponseEntity<>(menuService.addMenu(menu),HttpStatus.OK);
+        }
+        catch (IllegalArgumentException e){
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    public ResponseEntity<Menu> modifyMenu(@RequestBody Menu meal)
+            throws InstantiationException, IllegalAccessException{
+        Optional<Menu> originalMeal = menuService.getMenuById(meal.getId());
+        if(!originalMeal.isPresent())
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        Menu finalMeal = MergeTool.mergeObjects(meal,originalMeal.get());
+        try {
+            return new ResponseEntity<>(menuService.modifyMenu(finalMeal),HttpStatus.OK);
+        }
+        catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
---
+++ server/src/main/java/com/mimosa/menu/repository/MenuRepository.java
@@ -0,0 +1,7 @@
+package com.mimosa.menu.repository;
+
+import com.mimosa.menu.model.Menu;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MenuRepository extends CrudRepository<Menu,Integer>{
+}
