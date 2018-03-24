32afb0545fb58147ed246818c1883e7b97d2da7a
MilBia <218295@student.pwr.wroc.pl>
2018-03-24 15:27:10 +0100

    remove old folder placement

--- server/src/main/java/com/mimosa/order/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mimosa.order.repository;
-
-import com.mimosa.order.model.Order;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderRepository extends CrudRepository<Order,Integer>{
-}
--- server/src/main/java/com/mimosa/table/repository/TableRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mimosa.table.repository;
-
-import com.mimosa.table.model.Table;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TableRepository extends CrudRepository<Table,Integer>{
-}
