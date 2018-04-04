b9bc0dcde9270121c297d79299b2dddadb370552
MilBia <218295@student.pwr.wroc.pl>
2018-04-04 19:49:45 +0200

    Table module second iteration

--- projects/server/src/main/java/com/shusi/table/TableService.java
+++ projects/server/src/main/java/com/shusi/table/TableService.java
@@ -1,7 +1,17 @@
 package com.shusi.table;
 
+import com.shusi.table.model.Table;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Optional;
 
 @Service
 public interface TableService {
+
+    Collection<Table> getAllTables();
+
+    Optional<Table> getTable(Integer tableId);
+
+    Table addTable(Table table) throws IllegalArgumentException;
 }
