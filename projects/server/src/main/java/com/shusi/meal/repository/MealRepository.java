01ce27aa6e32904e6ffea826ce40e172dd2e3958
MilBia <218295@student.pwr.wroc.pl>
2018-04-04 23:02:28 +0200

    Ingredient service changes to meal connection

--- projects/server/src/main/java/com/shusi/meal/repository/MealRepository.java
+++ projects/server/src/main/java/com/shusi/meal/repository/MealRepository.java
@@ -1,7 +1,17 @@
 package com.shusi.meal.repository;
 
 import com.shusi.meal.model.Meal;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MealRepository extends CrudRepository<Meal, Integer>{
+
+    @Query("Select m" +
+            " FROM Meal m" +
+            " JOIN m.ingredients i" +
+            " WHERE i.id = :ingredeint_id")
+    List<Meal> getMealsByIngredient(@Param("ingredeint_id") Integer ingredeint_id);
 }
