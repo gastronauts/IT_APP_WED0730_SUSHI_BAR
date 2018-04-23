0a14a3f95d52a921b1e887baf053c7f5cf5e1b1f
Radoslaw <radek95gg@gmail.com>
2018-04-23 08:26:52 +0200

    Init Cheef app with configuartion v2

---
+++ projects/mobile/cheef_app/app/src/main/java/com/example/radoslaw/shushiapp_cheef/rest/SushiAppApiService.java
@@ -0,0 +1,10 @@
+package com.example.radoslaw.shushiapp_cheef.rest;
+
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import retrofit2.Call;
+
+public interface SushiAppApiService {
+
+}
