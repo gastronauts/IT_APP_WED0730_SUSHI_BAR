f682bd07b96db84ec77a23ea1029c39be69cb0c0
MilBia <milosz.bialczak@gmail.com>
2018-05-07 20:33:21 +0200

    Server iteration three
    * change properTime to integer
    
    * amount of meals in order
    
    * adding date to order
    
    * repair order modifying
    
    * leatle changes to initial SQL
    
    * removind id from SQL script for repair identification problem
    
    * deleting meals from menu endpoint
    
    * deleting ingredients from meal

--- projects/server/src/main/java/com/shusi/menu/impl/MenuImpl.java
+++ projects/server/src/main/java/com/shusi/menu/impl/MenuImpl.java
@@ -1,5 +1,6 @@
 package com.shusi.menu.impl;
 
+import com.shusi.meal.model.Meal;
 import com.shusi.menu.MenuService;
 import com.shusi.menu.model.Menu;
 import com.shusi.menu.repository.MenuRepository;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Optional;
 
@@ -53,6 +55,27 @@ public class MenuImpl implements MenuService{
                 throw new IllegalArgumentException(e);
             }
         } else
+            throw new IllegalArgumentException("Menu does not exist");
+    }
+
+    @Override
+    public Menu deleteMealFromMenu(Menu menu, Collection<Integer> meals) throws IllegalArgumentException {
+        if (menuRepository.exists(menu.getId())){
+            try {
+                Collection<Meal> mealCollection = menu.getMeals();
+                Collection<Meal> newMealCollection = new ArrayList<>();
+                mealCollection.stream().forEach(meal ->
+                {
+                    if (!meals.stream().anyMatch(id -> id.equals(meal.getId())))
+                        newMealCollection.add(meal);
+                });
+                menu.setMeals(newMealCollection);
+                return menuRepository.save(menu);
+            }
+            catch (DataAccessException e) {
+                throw new IllegalArgumentException(e);
+            }
+        }else
             throw new IllegalArgumentException("Menu does not exist");
     }
 
